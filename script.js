0b234ccbf2f3468f4703289b08488c6f9a9dbdfc
MehaboobS <sameernaseema175@gmail.com>
2024-07-14 06:34:23 -0700

    code

---
+++ script.js
@@ -0,0 +1,46 @@
+var num=45
+console.log(num)
+
+let str = "hello wordl"
+console.log(str)
+
+const isDone = true
+console.log(isDone)
+
+const arr = [1.34, "none", true, { 1: 2 }, [123, 456]]
+console.log(arr)
+const domObject = {
+    html: "complete Page",
+    body: "tags",
+    src: "js-logic file",
+    style:"css logic file"
+}
+console.log(domObject)
+
+let brr = 34
+console.log(brr)
+brr = 23
+console.log(brr)
+
+const still = 45
+
+console.log(still)
+
+console.log(typeof still,typeof domObject,typeof arr ,typeof isDone)
+
+let a = 4
+let b = 5
+console.log(a + b, a - b, a * b, a / b, a % b)
+
+a += 4
+console.log(a)
+b -= 1
+a = '8'
+b = 8
+console.log(a == b, a === b)
+
+console.log(a == b && a === b)
+console.log(a == b || a === b)
+
+result = a > 0 ? true : false
+console.log(result)
